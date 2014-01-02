9ffa57d18a725f68d4d29843db01e5a81ab3d4be
AlexMalinin <amalininhere@gmail.com>
2014-01-02 21:16:31 +0700

    Добавил выбор файлов. Добавил настройку шрифра.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,17 +1,69 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-    ui->gridLayout->setMargin(10);
-    ui->gridLayout->addWidget(ui->textEdit);
-    ui->centralWidget->setLayout(ui->gridLayout);
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent),
+      default_font_(QFont("Ubuntu Mono", 12)),
+      ui_(new Ui::MainWindow),
+      font_dialog_(new QFontDialog(this)),
+      left_file_dialog_(new QFileDialog(this)),
+      right_file_dialog_(new QFileDialog(this)) {
+  ui_->setupUi(this);
+  ui_->mainLayout->setMargin(10);
+  ui_->centralWidget->setLayout(ui_->mainLayout);
+  SetDefautFont();
+  connect(font_dialog_, SIGNAL(fontSelected(QFont)),
+          this, SLOT(ChangeFont(QFont)));
+  connect(left_file_dialog_, SIGNAL(fileSelected(QString)),
+          this, SLOT(OpenLeftFile(QString)));
+  connect(right_file_dialog_, SIGNAL(fileSelected(QString)),
+          this, SLOT(OpenRightFile(QString)));
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
+MainWindow::~MainWindow() {
+  delete ui_;
+  delete font_dialog_;
+  delete left_file_dialog_;
+  delete right_file_dialog_;
+}
+
+void MainWindow::ChangeFont(const QFont& font) {
+  ui_->leftText->setFont(font);
+  ui_->rightText->setFont(font);
+}
+
+void MainWindow::on_actionChangeFont_activated() {
+  font_dialog_->show();
+}
+
+void MainWindow::on_actionDefaultFont_activated() {
+  SetDefautFont();
+}
+
+void MainWindow::OpenLeftFile(const QString& file_name) {
+  ui_->leftFileName->setText(file_name);
+  if (0 != ui_->rightFileName->text().size()) {
+    ui_->compareButton->setEnabled(true);
+  }
+}
+
+void MainWindow::OpenRightFile(const QString& file_name) {
+  ui_->rightFileName->setText(file_name);
+  if (0 != ui_->leftFileName->text().size()) {
+    ui_->compareButton->setEnabled(true);
+  }
+}
+
+void MainWindow::on_leftOpenFileButton_clicked(){
+  left_file_dialog_->show();
+}
+
+void MainWindow::on_rightOpenFileButton_clicked() {
+  right_file_dialog_->show();
+}
+
+void MainWindow::SetDefautFont() {
+  font_dialog_->setCurrentFont(default_font_);
+  ui_->leftText->setFont(default_font_);
+  ui_->rightText->setFont(default_font_);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,21 +2,38 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFontDialog>
+#include <QFileDialog>
+#include <QFont>
 
 namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+  explicit MainWindow(QWidget *parent = 0);
+  ~MainWindow();
+
+private slots:
+  void ChangeFont(const QFont& font);
+  void on_actionChangeFont_activated();
+  void on_actionDefaultFont_activated();
+  void OpenLeftFile(const QString& file_name);
+  void OpenRightFile(const QString& file_name);
+  void on_leftOpenFileButton_clicked();
+  void on_rightOpenFileButton_clicked();
 
 private:
-    Ui::MainWindow *ui;
+  void SetDefautFont();
+
+  QFont default_font_;
+  Ui::MainWindow* ui_;
+  QFontDialog* font_dialog_;
+  QFileDialog* left_file_dialog_;
+  QFileDialog* right_file_dialog_;
 };
 
 #endif // MAINWINDOW_H
